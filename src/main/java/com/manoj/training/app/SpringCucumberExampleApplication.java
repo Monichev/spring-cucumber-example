6866142969b550a3f4d2378fbf8a692739bb2901
Manoj Verma <manojkumarhverma@gmail.com>
2019-04-14 09:51:24 +0530

    intial-commit

---
+++ src/main/java/com/manoj/training/app/SpringCucumberExampleApplication.java
@@ -0,0 +1,13 @@
+package com.manoj.training.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCucumberExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCucumberExampleApplication.class, args);
+	}
+
+}

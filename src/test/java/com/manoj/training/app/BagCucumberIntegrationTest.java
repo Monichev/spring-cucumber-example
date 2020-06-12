9a03816d7e62a4326a96c8c034d4aa2d3125d68b
Sergey Monichev <sergey.monichev@gmail.com>
2020-06-12 17:02:35 +0300

    Fix a typo

---
+++ src/test/java/com/manoj/training/app/BagCucumberIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.manoj.training.app;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features/bag.feature", plugin = {"pretty", "html:target/cucumber"})
+public class BagCucumberIntegrationTest {
+
+}
